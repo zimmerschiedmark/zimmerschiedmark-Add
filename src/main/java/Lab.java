ef40f22746f4e85bc4ed487f9c2c7440f5a0fae3
zimmerschiedmark <zimmerschiedmark@gmail.com>
2023-02-14 20:52:29 +0000

    Completed Coding Lab Add

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -11,6 +11,7 @@ public class Lab {
      * @return the sum of a and b.
      */
     public int addNumbers(int a, int b){
-        return 0;
+        int sum = a + b;
+        return sum;
     }
 }
